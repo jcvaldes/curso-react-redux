4f57648e8d81a3b4bb79bc6e8880a3a8b754bfd2
Juan Carlos Valdés <jcvaldes@combinatoria.com.ar>
2017-09-11 17:49:18 -0300

    React Redux

---
+++ exercicios_redux/ex/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Field from './field'
+import { combineReducers, createStore} from 'redux'
+import { Provider } from 'react-redux'
+
+const reducers  = combineReducers({
+  field: () => ({
+    value: 'Opa'
+  })
+})
+
+ReactDOM.render(
+   <Provider store={createStore(reducers)}>
+     <Field initialValue="Test" />
+   </Provider>
+    , document.getElementById('app'))
+
